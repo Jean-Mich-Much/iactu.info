8aa861afacd26fb42aa781f657a58979fa70ef6e
Jean-Michel GURGUI <jemi33@gmail.com>
2026-06-10 22:41:32 +0200

    Add files via upload

--- Fondation/php/messages_bot.php
+++ Fondation/php/messages_bot.php
@@ -1,35 +1,47 @@
 <!-- 😊 🤗 😄 😅 😀 🙂 😁 😇 😍 🥳 🥰 😜 🤩 😋 😏 😃 😆 -->
 
-<?php $affiche_message_bot_1 = false; ?>
-<?php if ($affiche_message_bot_1) { ?>
 <div>
 <span>
-🪧 Coucou
-</span></div><?php }?>
- 
-<?php $affiche_message_bot_2 = false; ?>
-<?php if ($affiche_message_bot_2) { ?>
-<div>
-<span>
-🪧 Coucou
-</span></div><?php }?>
- 
-<?php $affiche_message_bot_3 = true; ?>
-<?php if ($affiche_message_bot_3) { ?>
-<div>
-<span>
-🔔&nbsp;Abonnés&nbsp;: Arnaud&nbsp;C.&nbsp;5€, Alain&nbsp;S.&nbsp;5€,  Johann&nbsp;V.&nbsp;20€, Philippe&nbsp;B.&nbsp;5€, Philippe&nbsp;D.&nbsp;10€, Nicolas&nbsp;B.&nbsp;5€, Xavier&nbsp;M.&nbsp;5€, Sébastien&nbsp;D.&nbsp;5€&nbsp;et&nbsp;Damien&nbsp;V.&nbsp;5€
-</span></div><?php }?>
- 
-<?php $affiche_message_bot_4 = true; ?>
-<?php if ($affiche_message_bot_4) { ?>
-<div>
-<span>
-<a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" title=" Github 😊 " rel="noopener" id="a01">🐱&nbsp;Github </a> &nbsp;&nbsp; 
-<a href="https://codeberg.org/iActuinfo/iActu" target="_blank" aria-label="Codeberg 😊" title=" Codeberg 😊 " rel="noopener" id="a02">📜&nbsp;Codeberg </a> &nbsp;&nbsp; 
-<a href="https://iactu.info/Fondation/doc/index.html" target="_blank" aria-label="Doc 😊" title=" Doc 😊 " rel="noopener" id="a03">📚&nbsp;Doc </a> &nbsp;&nbsp; 
-<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-bot">✨&nbsp;Faire un don&nbsp; </a> &nbsp;&nbsp; 
-<a href="https://iactu.info/Page_stats.php" aria-label="Statistiques des pages 😊" title=" Statistiques des pages 😊 " rel="noopener" id="stats">&nbsp;🗃️&nbsp;Stats </a> &nbsp;&nbsp; 
-<a href="Papoter.php" aria-label="Papoter 😊" title=" Papoter 😊 " rel="noopener" id="Papoter-bot">&nbsp;🗨️&nbsp;Papoter </a> &nbsp;&nbsp; 
+<a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" title=" Github 😊 " rel="noopener" id="a01">&nbsp;🐱 Github&nbsp;</a> &nbsp;
+
+<a href="https://codeberg.org/iActuinfo/iActu" target="_blank" aria-label="Codeberg 😊" title=" Codeberg 😊 " rel="noopener" id="a02">&nbsp;📜 Codeberg&nbsp;</a> &nbsp;
+
+<a href="https://iactu.info/Fondation/doc/index.html" target="_blank" aria-label="Doc 😊" title=" Doc 😊 " rel="noopener" id="a03">&nbsp;📚 Doc&nbsp;</a> &nbsp;
+
+<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-bot">&nbsp;✨ Faire un don&nbsp;</a> &nbsp;
+
+<label for="abonnes">&nbsp;🔔 Abonnés :&nbsp;</label>
+<select id="abonnes" name="abonnes" style="background:var(--c3);color:var(--c4);border:0.125rem solid var(--c9);border-radius:0.25rem;">
+  <option value="johann-v">Johann V. 20€</option>
+  <option value="philippe-d">Philippe D. 10€</option>
+  <option value="arnaud-c">Arnaud C. 5€</option>
+  <option value="alain-s">Alain S. 5€</option>
+  <option value="philippe-b">Philippe B. 5€</option>
+  <option value="nicolas-b">Nicolas B. 5€</option>
+  <option value="xavier-m">Xavier M. 5€</option>
+  <option value="sebastien-d">Sébastien D. 5€</option>
+  <option value="damien-v">Damien V. 5€</option>
+</select> &nbsp;
+
+<label for="donateurs">&nbsp;🪧 Derniers donateurs :&nbsp;</label>
+<select id="donateurs" name="donateurs" style="background:var(--c3);color:var(--c4);border:0.125rem solid var(--c9);border-radius:0.25rem;">
+  <option value="ludovic-b">Ludovic B. 10€</option>
+  <option value="gregory-c">Grégory C. 5€</option>
+  <option value="emmanuel-s">Emmanuel S. 5€</option>
+  <option value="benoit-s">Benoît S. 5€</option>
+  <option value="philippe-rg">Philippe R. G. 10€</option>
+  <option value="edlenn-b">Edlenn B. 5€</option>
+  <option value="anthony-v">Anthony V. 10€</option>
+  <option value="david-n">David N. 5€</option>
+  <option value="philippe-v">Philippe V. 5€</option>
+  <option value="frederic-l">Frédéric L. 5€</option>
+  <option value="david-s">David S. 5€</option>
+  <option value="kevin-d">Kevin D. 10€</option>
+  <option value="loic-c">Loïc C. 5€</option>
+  <option value="sebastien-l">Sébastien L. 20€</option>
+  <option value="allan-v">Allan V. 5€</option>
+  <option value="olivier-p">Olivier P. 10€</option>
+</select> &nbsp;
+
 <span id="coucou" class="coucou" data-user="33imej" data-website="moc.liamg" title=" Coucou 😊 " aria-label="Coucou 😊" role="term"></span>
-</span></div><?php }
+</span></div>
--- Fondation/php/messages_top.php
+++ Fondation/php/messages_top.php
@@ -1,31 +1,13 @@
 <!-- 😊 🤗 😄 😅 😀 🙂 😁 😇 😍 🥳 🥰 😜 🤩 😋 😏 😃 😆 -->
 
-<!-- 
-
-&nbsp;<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-budget-top">&nbsp;📊&nbsp;Budget avril : manque 90€&nbsp; </a>
-
-&nbsp;<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-donateur-top">&nbsp;😇&nbsp;Dernier donateur : Philippe D. 10€ le 25 avril&nbsp; </a>
-
--->
-
-<?php $affiche_message_top_1 = true; ?>
-<?php if ($affiche_message_top_1) { ?>
+<?php $affiche_message_top = true; ?>
+<?php if ($affiche_message_top) { ?>
 <div>
 <span>
-🪧 Bienvenue !&nbsp;<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-budget-top">&nbsp;📊&nbsp;Budget avril : manque 90€&nbsp; </a>&nbsp;<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-top">&nbsp;✨&nbsp;Faire un don&nbsp; </a>&nbsp;<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-donateur-top">&nbsp;😇&nbsp;Dernier donateur : Philippe D. 10€ le 25 avril&nbsp; </a> &nbsp;<a href="https://iactu.info/Page_stats.php" aria-label="Statistiques des pages 😊" title=" Statistiques des pages 😊 " rel="noopener" id="stats-top">&nbsp;🗃️&nbsp;Stats </a> &nbsp;<a href="https://iactu.info/Fondation/doc/index.html" target="_blank" rel="noopener noreferrer" id="dochtml-top" aria-label="Documentation 😊" title=" Documentation 😊 ">&nbsp;📚&nbsp;Doc&nbsp;</a>&nbsp;<a href="Papoter.php" aria-label="Papoter 😊" title=" Papoter 😊 " rel="noopener" id="Papoter-top">&nbsp;🗨️&nbsp;Papoter&nbsp; </a>
+&nbsp;🪧 Bienvenue !&nbsp;
+
+&nbsp;<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-bot">&nbsp;✨ Faire un don&nbsp;</a> 
+
+&nbsp;<a href="https://iactu.info/Fondation/doc/index.html" target="_blank" rel="noopener noreferrer" id="dochtml-top" aria-label="Documentation 😊" title=" Documentation 😊 ">&nbsp;📚&nbsp;Doc&nbsp;</a> 
 
 </span></div><?php }?>
- 
-<?php $affiche_message_top_2 = false; ?>
-<?php if ($affiche_message_top_2) { ?>
-<div>
-<span>
-🪧 Coucou
-</span></div><?php }?>
- 
-<?php $affiche_message_top_3 = false; ?>
-<?php if ($affiche_message_top_3) { ?>
-<div>
-<span>
-🪧 Coucou
-</span></div><?php }
